a0d9ae291e9dc2a45c325f31df379fba852d13fe
Bugra Sen <bugra.sen@bil.omu.edu.tr>
2023-07-20 06:45:48 +0300

    Add project files.

---
+++ PersonalInformation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalInformationMethodInClass
+{
+    internal class PersonalInformation
+    {
+        public void personalInfo( string nameSurname, string job, int age)
+        {
+            Console.WriteLine("Name Surname : " + nameSurname+ " " + "Job : " + job + " " + "Age : " + age);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalInformationMethodInClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonalInformation personInfo = new PersonalInformation();
+
+            string nameSurname, job;
+            int age;
+
+            Console.WriteLine("Enter the Personal Information");
+
+            nameSurname = Console.ReadLine();
+            job = Console.ReadLine();
+            age = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+            personInfo.personalInfo(nameSurname, job, age);
+
+
+            Console.Read();
+        }
+    }
+}
